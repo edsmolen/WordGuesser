46274f38d85d24c38560a99ddeb6be92fb8a6a50
TradeIdeasPhilip <philip+github@trade-ideas.com>
2020-12-07 20:35:08 -0800

    Update WordGuesser.js
    Load words from dictionary.

--- docs/WordGuesser.js
+++ docs/WordGuesser.js
@@ -1,1 +1,19 @@
-alert("Hello world!");
+'use strict';
+
+let words;
+
+async function loadWords() {
+  const response = fetch("https://raw.githubusercontent.com/dwyl/english-words/master/words_alpha.txt");
+   if (!response.ok) {
+    throw new Error(`HTTP error! status: ${response.status}`);
+  }
+  const body = await response.text();
+  const result = new set();
+  body.split("\n").forEach(line => set.add(line));
+  return result;
+}
+
+async function initOnce() {
+  words = await loadWords();
+  alert("loaded " + words.size() + " words.");
+}
